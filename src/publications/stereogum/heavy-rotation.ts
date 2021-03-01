0a32f08031542e5ef8f21b9a26935bce9ccabcd0
my-slab <mitchell.stewart@mylsb.email>
2021-03-02 01:24:02 +1100

    Add Stereogum/Heavy Rotation

---
+++ src/publications/stereogum/heavy-rotation.ts
@@ -0,0 +1,39 @@
+import isEqual from 'lodash/isEqual';
+import uniqWith from 'lodash/uniqWith';
+import { Page } from 'puppeteer';
+
+import { Album, PublicationQuery } from '../../types';
+import { read, write } from '../../utils';
+
+const URL = 'https://www.stereogum.com/heavy-rotation';
+
+async function scrape(page: Page): Promise<Album[]> {
+  return page.evaluate((e) => {
+    const SELECTOR = ['.article-card__title', 'a'].join(' ');
+
+    let albums: Album[] = [];
+    for (let element of document.querySelectorAll(SELECTOR)) {
+      let [artist, title] = (element as HTMLElement).innerText.split('-');
+
+      albums.push({
+        artist: artist.trim(),
+        title: title.trim(),
+      });
+    }
+
+    return albums;
+  });
+}
+
+async function save(albums: Album[]) {
+  const PATH = './data/stereogum/heavy-rotation/albums.json';
+  let data = read(PATH);
+  data = JSON.stringify(uniqWith([...data, ...albums], isEqual));
+  write(PATH, data);
+}
+
+export const heavyRotation: PublicationQuery = {
+  URL,
+  save,
+  scrape,
+};
--- src/publications/stereogum/index.ts
+++ src/publications/stereogum/index.ts
@@ -1,1 +1,13 @@
-export * from './album-of-the-week';
+import { PublicationQuery } from '../../types';
+import { albumOfTheWeek } from './album-of-the-week';
+import { heavyRotation } from './heavy-rotation';
+
+export let stereogum = {
+  albumOfTheWeek,
+  heavyRotation,
+};
+
+export interface Stereogum {
+  albumOfTheWeek: PublicationQuery;
+  heavyRotation: PublicationQuery;
+}
