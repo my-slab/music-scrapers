13d98224c6d41a3249e1bcaee5b70203c1466179
Mitch Stewart <mitch.stewart@hey.com>
2021-04-17 17:19:21 +1000

    Update album-of-the-week.ts

--- src/publications/stereogum/album-of-the-week.ts
+++ src/publications/stereogum/album-of-the-week.ts
@@ -1,30 +1,30 @@
-import { Album, List } from '../../types'
-import { Page } from 'puppeteer'
-import { save } from '../../utils'
+import { Album, List } from '../../types';
+import { Page } from 'puppeteer';
 
-const PATH = './data/raw/stereogum/album-of-the-week.json'
-const URL = 'https://www.stereogum.com/category/reviews/album-of-the-week/'
+export const albumOfTheWeek: List = {
+  URL: 'https://www.stereogum.com/category/reviews/album-of-the-week/',
+  name: 'Album Of The Week',
+  save: () => {},
+  scrape,
+};
 
 async function scrape(page: Page): Promise<Album[]> {
   return page.evaluate(() => {
-    const SELECTOR = '.article-card__title a'
+    const SELECTOR = '.article-card__title a';
 
-    let albums: Album[] = []
+    let albums: Album[] = [];
     for (let { childNodes } of document.querySelectorAll(SELECTOR)) {
-      let [artist, title] = [childNodes[0] as ChildNode, childNodes[1] as HTMLElement]
+      let [artist, title] = [
+        childNodes[0] as ChildNode,
+        childNodes[1] as HTMLElement,
+      ];
 
       albums.push({
         artist: (artist.textContent || '').split('Album Of The Week:')[1],
         title: title.innerText,
-      })
+      });
     }
 
-    return albums
-  })
-}
-
-export const albumOfTheWeek: List = {
-  URL,
-  save: save(PATH),
-  scrape,
+    return albums;
+  });
 }
