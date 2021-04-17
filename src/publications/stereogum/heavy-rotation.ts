f7e05d11abb9db85607284cdeb1c93b323691271
Mitch Stewart <mitch.stewart@hey.com>
2021-04-17 17:19:42 +1000

    Update heavy-rotation.ts

--- src/publications/stereogum/heavy-rotation.ts
+++ src/publications/stereogum/heavy-rotation.ts
@@ -1,29 +1,26 @@
-import { Album, List } from '../../types'
-import { Page } from 'puppeteer'
-import { save } from '../../utils'
+import { Album, List } from '../../types';
+import { Page } from 'puppeteer';
 
-const PATH = './data/raw/stereogum/heavy-rotation.json'
-const URL = 'https://www.stereogum.com/heavy-rotation'
+export const heavyRotation: List = {
+  URL: 'https://www.stereogum.com/heavy-rotation',
+  name: 'Heavy Rotation',
+  save: () => {},
+  scrape,
+};
 
 async function scrape(page: Page): Promise<Album[]> {
   return page.evaluate(() => {
-    const SELECTOR = '.article-card__title a'
+    const SELECTOR = '.article-card__title a';
 
-    let albums: Album[] = []
+    let albums: Album[] = [];
     for (let element of document.querySelectorAll(SELECTOR)) {
-      let [artist, title] = (element as HTMLElement).innerText.split('-')
+      let [artist, title] = (element as HTMLElement).innerText.split('-');
       albums.push({
         artist: artist,
         title: title,
-      })
+      });
     }
 
-    return albums
-  })
-}
-
-export const heavyRotation: List = {
-  URL,
-  save: save(PATH),
-  scrape,
+    return albums;
+  });
 }
